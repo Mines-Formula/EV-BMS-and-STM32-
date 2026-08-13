c9354da26737ecd84d7e91a449c8946bffccc33a
neha <neha.college2025@gmail.com>
2026-08-13 13:48:02 -0600

    main with uart and printf commented out

--- Src/main.c
+++ Src/main.c
@@ -12,37 +12,37 @@
 #include "stm32f1xx_hal.h"
 #include "stm32f1xx_hal_gpio.h"
 #include "stm32f1xx_hal_rcc.h"
-#include "stm32f1xx_hal_uart.h"
+//#include "stm32f1xx_hal_uart.h" // UART HAL header
 
 // Global UART handle
-UART_HandleTypeDef huart2;
+// UART_HandleTypeDef huart2;
 
 // Function Prototypes
 void SystemClock_Config(void);
 void GPIO_Init(void);
-void USART2_UART_Init(void);
+// void USART2_UART_Init(void);
 
 int main(void){
     HAL_Init();
     SystemClock_Config();
     GPIO_Init();        
-    USART2_UART_Init();
+    // USART2_UART_Init();
 
     // Disable stdout buffering so printf sends characters immediately
-    setvbuf(stdout, NULL, _IONBF, 0);
+    // setvbuf(stdout, NULL, _IONBF, 0);
 
-    printf("\r\n--- STM32F103 USART2 Printf Initialized ---\r\n");
+    // printf("\r\n--- STM32F103 USART2 Printf Initialized ---\r\n");
 
     for(int i = 0; i < 10; i++)
     {
         // Toggle LED (PC15)
         HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_15);
-        printf("Loop count: %d\r\n", i + 1);
+        // printf("Loop count: %d\r\n", i + 1);
         
         HAL_Delay(500); // Replaced bare spin loop with HAL delay
     }
 
-    printf("Blink test complete!\r\n");
+    // printf("Blink test complete!\r\n");
 
     while(1) {
         // Main loop
@@ -50,47 +50,47 @@ int main(void){
 }
 
 // Low-level syscall retargeting for GCC Arm Toolchain
-int _write(int file, char *ptr, int len) {
-    HAL_UART_Transmit(&huart2, (uint8_t*)ptr, len, HAL_MAX_DELAY);
-    return len;
-}
+// int _write(int file, char *ptr, int len) {
+//     HAL_UART_Transmit(&huart2, (uint8_t*)ptr, len, HAL_MAX_DELAY);
+//     return len;
+// }
 
 // Configures USART2 (TX: PA2, RX: PA3) at 115200 8N1
-void USART2_UART_Init(void)
-{
-    __HAL_RCC_USART2_CLK_ENABLE();
-    __HAL_RCC_GPIOA_CLK_ENABLE();
+// void USART2_UART_Init(void)
+// {
+//     __HAL_RCC_USART2_CLK_ENABLE();
+//     __HAL_RCC_GPIOA_CLK_ENABLE();
 
-    // Configure GPIO Pins for USART2: PA2 (TX) & PA3 (RX)
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
+//     // Configure GPIO Pins for USART2: PA2 (TX) & PA3 (RX)
+//     GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-    // PA2 -> USART2_TX (Alternate Function Push-Pull)
-    GPIO_InitStruct.Pin = GPIO_PIN_2;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+//     // PA2 -> USART2_TX (Alternate Function Push-Pull)
+//     GPIO_InitStruct.Pin = GPIO_PIN_2;
+//     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+//     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+//     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-    // PA3 -> USART2_RX (Input Floating / Pull-Up)
-    GPIO_InitStruct.Pin = GPIO_PIN_3;
-    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+//     // PA3 -> USART2_RX (Input Floating / Pull-Up)
+//     GPIO_InitStruct.Pin = GPIO_PIN_3;
+//     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+//     GPIO_InitStruct.Pull = GPIO_NOPULL;
+//     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-    // Configure UART parameters
-    huart2.Instance = USART2;
-    huart2.Init.BaudRate = 115200;
-    huart2.Init.WordLength = UART_WORDLENGTH_8B;
-    huart2.Init.StopBits = UART_STOPBITS_1;
-    huart2.Init.Parity = UART_PARITY_NONE;
-    huart2.Init.Mode = UART_MODE_TX_RX;
-    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-    huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+//     // Configure UART parameters
+//     huart2.Instance = USART2;
+//     huart2.Init.BaudRate = 115200;
+//     huart2.Init.WordLength = UART_WORDLENGTH_8B;
+//     huart2.Init.StopBits = UART_STOPBITS_1;
+//     huart2.Init.Parity = UART_PARITY_NONE;
+//     huart2.Init.Mode = UART_MODE_TX_RX;
+//     huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+//     huart2.Init.OverSampling = UART_OVERSAMPLING_16;
 
-    if (HAL_UART_Init(&huart2) != HAL_OK)
-    {
-        while(1);
-    }
-}
+//     if (HAL_UART_Init(&huart2) != HAL_OK)
+//     {
+//         while(1);
+//     }
+// }
 
 // Clock Config (16MHz HSE)
 void SystemClock_Config(void)
